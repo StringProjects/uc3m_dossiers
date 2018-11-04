2791b21a9a610a7dabc607dcdf97749be9eeb2da
Carlos Muiño Migallon <camumino@gmail.com>
2018-11-04 12:33:39 +0100

    several changes

--- app/controllers/dossiers_controller.rb
+++ app/controllers/dossiers_controller.rb
@@ -3,5 +3,9 @@ class DossiersController < ApplicationController
   def index
     @dossiers = Dossier.all
   end
+
+  def show
+    @dossier = Dossier.find(params[:id])
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :dossiers, only: [:index]
+  resources :dossiers, only: [:index, :show]
 
   root 'dossiers#index'
 end
