dbe75e3f4ca4bde3159eddf4260008c7b56f8195
Carlos Muiño Migallon <camumino@gmail.com>
2019-04-27 20:41:49 +0200

    fixes

--- app/models/dossier.rb
+++ app/models/dossier.rb
@@ -2,7 +2,7 @@ class Dossier < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
   
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   
   validates_presence_of :name
